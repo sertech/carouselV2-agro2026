0330633d7b16d2ec74144907efc9168e2a7b99dd
Sergio Alvarez Pinto <salvarez@ine.gov.bo>
2026-04-17 16:40:28 -0400

    semi final version img, movement complete

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const carouselTrack = document.querySelector('.carousel-track');
 const carouselOverlays = document.querySelectorAll('.carousel-overlay');
 const trackTransitionMs = 700; // Should match the CSS transition duration for the track movement.
 const trackFrameTransitionMs = 1300; // Should match the CSS transition duration for the frame and overlay reveal.
-const carouselFrames = document.querySelectorAll('.carousel-frame');
+const carouselPortals = document.querySelectorAll('.carousel-portal');
+const carouselFrameImages = document.querySelectorAll('.carousel-frame-image')
 const carouselContents = document.querySelectorAll('.carousel-content');
 
 let pendingOverlayTimer; // Used to delay the overlay reveal until the slide transition finishes.
@@ -21,7 +22,7 @@ const setActiveSlide = (activeIndex) => {
     });
 
     // reset all frames to the default state before activating the new one
-    carouselFrames.forEach((frame) => {
+    carouselPortals.forEach((frame) => {
         frame.classList.remove('is-active');
     });
 
@@ -41,22 +42,22 @@ const setActiveSlide = (activeIndex) => {
     pendingOverlayTimer = window.setTimeout(() => {
         switch (activeIndex) {
             case 0:
-                carouselOverlays[activeIndex].style.background = 'rgba(47, 93, 58, 0.55)';
+                carouselOverlays[activeIndex].style.background = 'rgba(47, 93, 58, 0.25)';
                 break;
             case 1:
-                carouselOverlays[activeIndex].style.background = 'rgba(94, 156, 69, 0.55)';
+                carouselOverlays[activeIndex].style.background = 'rgba(94, 156, 69, 0.25)';
                 break;
             case 2:
-                carouselOverlays[activeIndex].style.background = 'rgba(217, 164, 65, 0.55)';
+                carouselOverlays[activeIndex].style.background = 'rgba(217, 164, 65, 0.25)';
                 break;
             case 3:
-                carouselOverlays[activeIndex].style.background = 'rgba(168, 93, 58, 0.55)';
+                carouselOverlays[activeIndex].style.background = 'rgba(168, 93, 58, 0.25)';
                 break;
             case 4:
-                carouselOverlays[activeIndex].style.background = 'rgba(216, 199, 161, 0.55)';
+                carouselOverlays[activeIndex].style.background = 'rgba(216, 199, 161, 0.25)';
                 break;
             case 5:
-                carouselOverlays[activeIndex].style.background = 'rgba(122,132,68,0.55)';
+                carouselOverlays[activeIndex].style.background = 'rgba(122,132,68,0.25)';
                 break;
             default:
                 carouselOverlays[activeIndex].style.background = 'rgba(76, 66, 124, 0.55)';
@@ -65,15 +66,31 @@ const setActiveSlide = (activeIndex) => {
     }, trackTransitionMs);
 
     pendingFrameTimer = window.setTimeout(() => {
-        // switch (activeIndex) {
-        //     case 0:
-        //         carouselFrames[activeIndex].style.background = 'rgba(47, 93, 58, 0.78)';
-        //         break;
-        // }
-        carouselFrames[activeIndex]?.classList.add('is-active');
+        carouselPortals[activeIndex]?.classList.add('is-active');
         carouselContents[activeIndex]?.classList.add('is-active');
     }, trackFrameTransitionMs);
 };
+
+carouselPortals.forEach((portal, index) => {
+    portal.addEventListener('mousemove', (event) => {
+        const rect = portal.getBoundingClientRect();
+
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const moveX = ((x - centerX) / centerX) * 80;
+        const moveY = ((y - centerY) / centerY) * 80;
+
+        carouselFrameImages[index].style.transform = `scale(1.5) translate(${moveX}px, ${moveY}px)`;
+    });
+
+    portal.addEventListener('mouseleave', () => {
+        carouselFrameImages[index].style.transform = `scale(1.08) translate(0,0)`
+    });
+})
 
 carouselButtons.forEach((button, index) => {
     button.addEventListener('click', (event) => {
